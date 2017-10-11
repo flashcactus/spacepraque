773aa1d1ff6fb6f99818f750c06a3ba1abbb09bb
Ivan Uvarov <flashcactus@ya.ru>
2017-10-11 14:05:41 +0300

    pep8ify everything

--- task1.py
+++ task1.py
@@ -1,20 +1,23 @@
 import re
 import mystuff
 
+
 def readmatr():
     mat = []
     while True:
         tokens = input().split()
         if not len(tokens):
             break
-        mat.append(list(map(float,tokens)))
-    minlen = min(map(len,mat))
+        mat.append(list(map(float, tokens)))
+    minlen = min(map(len, mat))
     return [row[:minlen] for row in mat]
 
-def trace(mat,offset):
-    return 
+
+def trace(mat, offset):
+    return
 
 cp = mystuff.CmdProcessor()
+
 
 @cp.regcmd('diagsum ([0-9]+)')
 def diagsum(*args):
--- task3.py
+++ task3.py
@@ -8,16 +8,22 @@ mcproc = mystuff.CmdProcessor()
 fdict = {}
 
 funs = {
-    'mul': lambda a,b: a * b,
-    'pow': lambda a,b: a ** b, 
-    'sin': lambda a,b: math.sin(a * math.pi + b)
+    'mul': lambda a, b: a * b,
+    'pow': lambda a, b: a ** b,
+    'sin': lambda a, b: math.sin(a * math.pi + b)
 }
+
 
 @mcproc.command("([a-zA-Z]+) *= *(mul|pow|sin) +([0-9.]+)")
 def add_function(match):
     name, fname, arg = match.groups()
+    if name in fdict:
+        confirmation = input(name + ' exists. overwrite? [Yn]: ').lower()
+    if confirmation == 'n':
+        break
     fdict[name] = (funs[fname], float(arg))
     return 'OK'
+
 
 @mcproc.command("([a-zA-Z]+)\(([0-9.]+)\)")
 def run_function(match):
@@ -25,14 +31,14 @@ def run_function(match):
     fun, funarg = fdict[name]
     return fun(funarg, float(arg))
 
+
 def read_cmd():
     cmd = input('> ')
     try:
         print(mcproc.run_cmd(cmd))
     except Exception as e:
-        print(type(e),e.args)
+        print(type(e), e.args)
 
 
-
-while True: 
+while True:
     read_cmd()
